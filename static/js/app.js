ffddeb8d322739fb89a3d0cfb74d7d0475fe1863
Jon Scheaffer <84929443+J-Schea29@users.noreply.github.com>
2021-10-05 17:28:51 -0700

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -6,6 +6,21 @@ function buildMetaData(sample) {
       return data.id === sample;
     })
     console.log(resultArray);
+
+    var personID = resultArray.id
+    console.log(personID);
+    var ethnicity = resultArray.ethnicity
+    console.log(ethnicity);
+    var gender = resultArray.gender
+    console.log(gender);
+    var age = resultArray.age
+    console.log(age);
+    var location = resultArray.location
+    console.log(location);
+    var bbtype = resultArray.bbtype
+    console.log(bbtype);
+    var wfreq = resultArray.wfreq
+    console.log(wfreq);
   })
 }
 
@@ -14,13 +29,59 @@ function buildCharts(sample) {
     var samples = data.samples;
     var resultArray = samples.filter(function(data){
       return data.id === sample;
-      var otu_ids = resultArray.otu_ids;
-      var otu_labels = resultArray.otu_labels;
-      var sample_values = resultArray.sample_values;
     })
-    console.log(resultArray);
+    var result = resultArray[0]
+    var otu_ids = result.otu_ids;
+    console.log(otu_ids)
+    var otu_labels = result.otu_labels;
+    console.log(otu_labels)
+    var sample_values = result.sample_values;
+    console.log(sample_values)
+
+    var yticks = otu_ids.slice(0, 10).map(function(otuID) {
+      return `OTU ${otuID}`;
+    }).reverse();
+    console.log(yticks)
+
+    // Charts
+    // Bar Chart
+    let barLayout = {
+      title: "Top 10 Bacteria Found",
+      margin: {
+        t:30, 
+        l: 150,
+      },
+
+    };
+    let barData = {
+      y: yticks,
+      x: sample_values.slice(0,10).reverse,
+      text: otu_labels.slice(0,10).reverse,
+      type: "bar",
+      orientation: "h"
+    };
+    Plotly.newPlot("bar", barData, barLayout);
+    // Bubble Chart
+    var bubbleLayout = {
+      title: "Bacteria Present in Subject",
+      havermode: "closest",
+      xaxis: { title: "OTU ID"},
+      margin: {t: 30}
+    };
+    var bubbleData = {
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        mode: "markers",
+        marker: {
+          size: sample_values,
+          color: otu_ids,
+          colorscale:"Earth"
+        },
+    };
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
   })
-}
+};
 
 function init() {
   //select in index.html where you want subject id info
@@ -49,34 +110,3 @@ function init() {
 init()
 
 
-
-
-// Sort the data by Greek search results descending
-let sortedByGreekSearch = data.sort((a, b) => b.greekSearchResults - a.greekSearchResults);
-
-// Slice the first 10 objects for plotting
-slicedData = sortedByGreekSearch.slice(0, 10);
-
-// Reverse the array to accommodate Plotly's defaults
-reversedData = slicedData.reverse();
-
-let trace1 = {
-  x: reversedData.map(object => object.greekSearchResults),
-  y: reversedData.map(object => object.greekName),
-  text: reversedData.map(object => object.greekName),
-  name: "Greek",
-  type: "bar",
-  orientation: "h"
-};
-
-let layout = {
-  title: "Greek gods search results",
-  margin: {
-    l: 100,
-    r: 100,
-    t: 100,
-    b: 100
-  }
-};
-
-Plotly.newPlot("plot", traceData, layout);
